e701d300e5adf1884ebd841da043bc84ecb0f456
音夢?! <82650472+Nemu627@users.noreply.github.com>
2022-05-07 21:09:41 +0900

    Update __init__.py

--- pythmc/aurora/__init__.py
+++ pythmc/aurora/__init__.py
@@ -1,3 +1,10 @@
-from . import nation_
-from . import town_
-from . import resident_
+from pythmc.nation import BaseNation
+from pythmc import get
+
+from typing import Tuple
+
+
+class Nation(BaseNation):
+    def __init__(self, name: str, *, data: Tuple[dict, dict] = None):
+        super().__init__(name, data)
+        self.get_data = get.get_data("aurora")
